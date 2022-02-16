e537c2e95bc29f1afa335989a1c34fec8e0bb00e
Patryk Kwapiszewski <patryk.kwapiszewski@wp.pl>
2022-02-16 10:21:39 +0100

    Commit

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass!");
+        System.out.println("Patryk");
+        System.out.println("Kwapiszewski");
+        System.out.println("K");
+
+    }
+    }

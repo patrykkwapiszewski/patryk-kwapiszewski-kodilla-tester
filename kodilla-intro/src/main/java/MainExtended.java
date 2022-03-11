b24187e220a8fe905ca7b947abc3417d783ca76d
Patryk Kwapiszewski <patryk.kwapiszewski@wp.pl>
2022-03-11 13:19:33 +0100

    Commit

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,12 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String myName = "Patryk";
+        String lastName = "Kwapiszewski";
+        Number myAge = 24;
+        boolean isMale = true;
+        char nameFirstLetter = 'P';
+        System.out.println(isMale);
+        System.out.println(nameFirstLetter);
+
+    }
+}

69c6d47a82baa63c37225a36eb79aea1f023e9cc
Patryk Kwapiszewski <patryk.kwapiszewski@wp.pl>
2022-03-20 10:21:47 +0100

    V2

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    private String author;
+    private String title;
+
+    static String of(String author, String title){
+        System.out.println("This book is written by: " + author);
+        return author;
+    }
+
+
+
+
+}
